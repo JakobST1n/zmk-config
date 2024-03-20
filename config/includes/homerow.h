0b4b798bb0788d6e9840cc7975fbe41cc39f18b9
jakobst1n <jakob.stendahl@outlook.com>
2024-03-20 18:34:48 +0100

    Add symbols

--- config/includes/homerow.h
+++ config/includes/homerow.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAKE_HRM(NAME, HOLD, TAP) \
+#define MAKE_HRM(NAME, HOLD, TAP, TRIGGER_POS) \
     / { \
       behaviours { \
 	NAME: NAME { \
@@ -12,11 +12,12 @@
 	  quick-tap-ms = <QUICK_TAP_MS>; \
 	  require-prior-idle-ms = <150>; \
 	  bindings = <HOLD>, <TAP>; \
+          hold-trigger-key-positions = <TRIGGER_POS>; \
 	  hold-trigger-on-release; \
 	}; \
       }; \
     };
-
-MAKE_HRM(hml, &kp, &kp)
-MAKE_HRM(hmr, &kp, &kp)
+ 
+MAKE_HRM(hml, &kp, &kp, KEYS_R THUMBS)
+MAKE_HRM(hmr, &kp, &kp, KEYS_L THUMBS)
 
